2e7e3b39d1263acfd1c2cc14d003e4061e6ddfc4
Connor <moorman.40@wright.edi>
2015-10-25 14:54:15 -0400

    Update MAIN

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -11,26 +11,17 @@ public class Main {
 	public static CheckerBoard GameWindow;
 	public List<Checker> Checkers = new ArrayList<Checker>();
 	
-	public static void main(String[] args) {
-		SetupGameWindow();
-	}
-	
-	private static void SetupGameWindow()
-	{
-		//GameWindow = new CheckerBoard();
-		
-		JFrame mainFrame = new JFrame("GameFrame");
-		mainFrame.setContentPane(new CheckerBoard());
-		mainFrame.setResizable(true);
-		mainFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		mainFrame.pack();
-		mainFrame.setVisible( true );
-	}
 
 	/* (non-Java-doc)
 	 * @see java.lang.Object#Object()
 	 */
-	public Main() {
-		super();
+	public static void main(String[] args)
+	{
+		JFrame window = new JFrame("Digital Checkers");		
+		window.setContentPane( new GamePanel() );		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+		window.pack();
+		window.setVisible(true);
 	}
 }
