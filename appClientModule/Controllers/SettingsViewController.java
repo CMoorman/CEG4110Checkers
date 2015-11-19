2b21ce90b40e86d34493ebb6f34861381a2f5594
Connor <moorman.40@wright.edi>
2015-11-19 00:45:43 -0500

    Updating theSettingsView FXML to have text fields.  Added in some logic for hex only values.

--- appClientModule/Controllers/SettingsViewController.java
+++ appClientModule/Controllers/SettingsViewController.java
@@ -8,6 +8,8 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.WindowEvent;
 
@@ -16,10 +18,28 @@ public class SettingsViewController extends BaseView implements Initializable{
 	@FXML
 	Pane settingsPane;
 	
+	@FXML
+	TextField titleTxtField = new TextField();
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
+		
 		
 	}
-
+	
+	@FXML
+	public void checkInputLength(KeyEvent ev){
+		
+		try {
+			// -- Make sure that we have the correct input and input length.
+			TextField currentField = (TextField) ev.getSource();
+			if( "1234567890abcdefABCDEF".contains( ev.getCharacter() ) && (currentField.getText().length() < 6 ) ) { 
+				// -- Empty if.  For some reason why I try to throw some nots in, it does not work.
+			}
+			else
+				ev.consume();
+		}catch(Exception e){
+			// -- Need a try catch here.  If the textfield does not have any text in it initially, throws an error
+		}
+	}
 }
