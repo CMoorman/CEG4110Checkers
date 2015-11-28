5f960d77274eea89e9f0ab0527717e72f6cc8b21
Connor <moorman.40@wright.edi>
2015-11-28 15:55:26 -0500

    Throwing in the CheckersBoardViewController class.

---
+++ appClientModule/Controllers/CheckersBoardViewController.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import UIPanes.BaseView;
+
+public class CheckersBoardViewController extends BaseView implements Initializable, BaseViewController {
+
+	@Override
+	public void ButtonClicked(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
