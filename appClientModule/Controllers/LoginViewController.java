b3c165eeaa2cc382e98bcd8cfdb97b5ad5f9712e
Brian <Brian@10.211.55.4>
2015-11-30 20:18:20 -0500

    add in textbox from xml Javafx

--- appClientModule/Controllers/LoginViewController.java
+++ appClientModule/Controllers/LoginViewController.java
@@ -24,6 +24,9 @@ public class LoginViewController extends BaseView implements Initializable {
 	TextField usernameTxtField;
 	
 	@FXML
+	TextField ipAddressTxtField;
+	
+	@FXML
 	Button submitBtn;
 	
 	@FXML
