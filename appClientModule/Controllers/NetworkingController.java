bdf452c7c0a7fed91fe2e0e10a644b0d7c328f9a
Brian <Brian@10.211.55.4>
2015-12-03 21:26:14 -0500

    Add in error message for bad names and names taken

--- appClientModule/Controllers/NetworkingController.java
+++ appClientModule/Controllers/NetworkingController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javafx.application.Platform;
 import Networking.CheckersClient;
 import Networking.ServerCommunicator;
+import Objects.DialogHelper;
 import Objects.TableListObject;
 import UIPanes.BaseView;
 
@@ -193,37 +194,43 @@ public class NetworkingController extends BaseView implements CheckersClient {
 	@Override
 	public void yourTurn() {
 		// TODO Auto-generated method stub
-		
+		// allow user to move peice
+		// write "your turn" to text box?
 	}
 
 	@Override
 	public void nowObserving(int tid) {
 		// TODO Auto-generated method stub
-		
+		// revoke moving peices capabilities
+		// remove concede button?
 	}
 
 	@Override
 	public void stoppedObserving(int tid) {
 		// TODO Auto-generated method stub
-		
+		// change scene?
 	}
 
 	@Override
 	public void networkException(String msg) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Hmm, not sure. Feels Bad Man.");
 	}
 
 	@Override
 	public void nameInUseError() {
 		// TODO Auto-generated method stub
-		
+		LoginViewController controller = LoginViewController.getInstance();
+		controller.nameAvailable=false;
+		controller.loginSuccess=false;
 	}
 
 	@Override
 	public void nameIllegal() {
 		// TODO Auto-generated method stub
-		
+		LoginViewController controller = LoginViewController.getInstance();
+		controller.nameIllegal=true;
+		controller.loginSuccess=false;
 	}
 
 	@Override
